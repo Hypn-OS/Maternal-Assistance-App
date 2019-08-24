fd50c8ca356fdc625dda6a0c1253a485d75bed63
Kaushlendra Pratap <kaushlendrapratap.9837@gmail.com>
2019-08-25 02:53:47 +0530

    Update login page

--- lib/profile_page.dart
+++ lib/profile_page.dart
@@ -44,7 +44,7 @@ class MapScreenState extends State<ProfilePage>
                             ),
                             Padding(
                               padding: EdgeInsets.only(left: 25.0),
-                              child: new Text('PROFILE',
+                              child: new Text('Background Information',
                                   style: TextStyle(
                                       fontWeight: FontWeight.bold,
                                       fontSize: 20.0,
@@ -113,7 +113,7 @@ class MapScreenState extends State<ProfilePage>
                                 mainAxisSize: MainAxisSize.min,
                                 children: <Widget>[
                                   new Text(
-                                    'Parsonal Information',
+                                    'Personal Information',
                                     style: TextStyle(
                                         fontSize: 18.0,
                                         fontWeight: FontWeight.bold),
@@ -140,7 +140,7 @@ class MapScreenState extends State<ProfilePage>
                                 mainAxisSize: MainAxisSize.min,
                                 children: <Widget>[
                                   new Text(
-                                    'Name',
+                                    'Expected Delivery Date',
                                     style: TextStyle(
                                         fontSize: 16.0,
                                         fontWeight: FontWeight.bold),
@@ -158,7 +158,7 @@ class MapScreenState extends State<ProfilePage>
                               new Flexible(
                                 child: new TextField(
                                   decoration: const InputDecoration(
-                                    hintText: "Enter Your Name",
+                                    hintText: "Enter Date in DD/MM/YYYY format",
                                   ),
                                   enabled: !_status,
                                   autofocus: !_status,
@@ -178,7 +178,7 @@ class MapScreenState extends State<ProfilePage>
                                 mainAxisSize: MainAxisSize.min,
                                 children: <Widget>[
                                   new Text(
-                                    'Email ID',
+                                    'Age',
                                     style: TextStyle(
                                         fontSize: 16.0,
                                         fontWeight: FontWeight.bold),
@@ -196,7 +196,7 @@ class MapScreenState extends State<ProfilePage>
                               new Flexible(
                                 child: new TextField(
                                   decoration: const InputDecoration(
-                                      hintText: "Enter Email ID"),
+                                      hintText: "Enter your age"),
                                   enabled: !_status,
                                 ),
                               ),
@@ -242,12 +242,47 @@ class MapScreenState extends State<ProfilePage>
                               left: 25.0, right: 25.0, top: 25.0),
                           child: new Row(
                             mainAxisSize: MainAxisSize.max,
+                            children: <Widget>[
+                              new Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                mainAxisSize: MainAxisSize.min,
+                                children: <Widget>[
+                                  new Text(
+                                    'Pincode',
+                                    style: TextStyle(
+                                        fontSize: 16.0,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          )),
+                      Padding(
+                          padding: EdgeInsets.only(
+                              left: 25.0, right: 25.0, top: 2.0),
+                          child: new Row(
+                            mainAxisSize: MainAxisSize.max,
+                            children: <Widget>[
+                              new Flexible(
+                                child: new TextField(
+                                  decoration: const InputDecoration(
+                                      hintText: "Enter your pincode"),
+                                  enabled: !_status,
+                                ),
+                              ),
+                            ],
+                          )),
+                      Padding(
+                          padding: EdgeInsets.only(
+                              left: 25.0, right: 25.0, top: 25.0),
+                          child: new Row(
+                            mainAxisSize: MainAxisSize.max,
                             mainAxisAlignment: MainAxisAlignment.start,
                             children: <Widget>[
                               Expanded(
                                 child: Container(
                                   child: new Text(
-                                    'Pin Code',
+                                    'Height',
                                     style: TextStyle(
                                         fontSize: 16.0,
                                         fontWeight: FontWeight.bold),
@@ -258,7 +293,7 @@ class MapScreenState extends State<ProfilePage>
                               Expanded(
                                 child: Container(
                                   child: new Text(
-                                    'State',
+                                    'Weight',
                                     style: TextStyle(
                                         fontSize: 16.0,
                                         fontWeight: FontWeight.bold),
@@ -280,7 +315,7 @@ class MapScreenState extends State<ProfilePage>
                                   padding: EdgeInsets.only(right: 10.0),
                                   child: new TextField(
                                     decoration: const InputDecoration(
-                                        hintText: "Enter Pin Code"),
+                                        hintText: "in cms"),
                                     enabled: !_status,
                                   ),
                                 ),
@@ -289,7 +324,7 @@ class MapScreenState extends State<ProfilePage>
                               Flexible(
                                 child: new TextField(
                                   decoration: const InputDecoration(
-                                      hintText: "Enter State"),
+                                      hintText: "in kgs"),
                                   enabled: !_status,
                                 ),
                                 flex: 2,
--- lib/ui/login_page.dart
+++ lib/ui/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:Maternal/pages/dashboard.dart';
 import 'package:Maternal/profile_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/material.dart' as prefix0;
@@ -5,7 +6,7 @@ import 'package:flutter/services.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:Maternal/style/theme.dart' as Theme;
 import 'package:Maternal/utils/bubble_indication_painter.dart';
-import 'package:Maternal/profile_page.dart';
+
 
 class LoginPage extends StatefulWidget {
   LoginPage({Key key}) : super(key: key);
@@ -337,7 +338,7 @@ class _LoginPageState extends State<LoginPage>
                     ),
                     onPressed: () =>
                         Navigator.pushReplacement(context, prefix0.MaterialPageRoute(
-                          builder: (context) => ProfilePage()
+                          builder: (context) => MainPage()
                         ))
               ),
 
@@ -642,7 +643,10 @@ class _LoginPageState extends State<LoginPage>
                       ),
                     ),
                     onPressed: () =>
-                        showInSnackBar("SignUp button pressed")),
+                      Navigator.pushReplacement(context, prefix0.MaterialPageRoute(
+                          builder: (context) => ProfilePage()
+                        ))
+                ),        
               ),
             ],
           ),

b37f44f651dfcca8ffc546cac97fcb4a89fbd83c
sahil <sjha200000@gmail.com>
2019-08-24 12:49:38 +0530

    splash screen

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:maa/main.dart';
+import 'package:flutter_native_splash/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
